9528fe3b3519a88beb315d532827d72fef4606aa
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-12-09 18:01:02 +0100

    add account hook

--- frontend/packages/nextjs/pages/create-event.tsx
+++ frontend/packages/nextjs/pages/create-event.tsx
@@ -1,4 +1,5 @@
 import { FormEvent, useState } from "react";
+import { useAccount } from "wagmi";
 import { Button } from "~~/components/Button";
 
 type FormData = {
@@ -11,6 +12,8 @@ type FormData = {
 };
 
 const CreateEvent = () => {
+  const { isDisconnected } = useAccount();
+
   const [formData, setFormData] = useState<FormData>({
     eventName: undefined,
     eventDate: undefined,
@@ -54,55 +57,61 @@ const CreateEvent = () => {
   };
   return (
     <div className="select-none text-center md:mx-10 lg:mx-16 xl:mx-48 2xl:mx-96 mt-10">
-      <h1 className="font-orbitron">Create Event</h1>
-      <form onSubmit={submitHandler} className="flex flex-col items-center gap-2 my-8">
-        <div className="flex flex-col gap-2 w-[80%]">
-          <input
-            required
-            onChange={inputHandler}
-            className="input text-gray-400"
-            name="eventName"
-            type="text"
-            placeholder="Event Name"
-          />
-          <input
-            onChange={inputHandler}
-            className="input text-gray-400"
-            name="eventDate"
-            type="date"
-            placeholder="Event Date"
-          />
-          <input
-            onChange={inputHandler}
-            className="input text-gray-400"
-            name="ticketPrice"
-            type="number"
-            placeholder="Ticket Price"
-          />
-          <input
-            onChange={inputHandler}
-            className="input text-gray-400"
-            name="ticketQuantity"
-            type="number"
-            placeholder="Ticket Quantity"
-          />
-          <input
-            onChange={inputHandler}
-            className="input text-gray-400"
-            name="artistName"
-            type="text"
-            placeholder="Artist Name"
-          />
-          <input
-            onChange={inputHandler}
-            className="input text-gray-400"
-            name="artistAddress"
-            type="text"
-            placeholder="Artist Wallet Address"
-          />
-        </div>
-        <Button full>Create</Button>
-      </form>
+      {isDisconnected ? (
+        <p className="text-lg">Please connect to a wallet!</p>
+      ) : (
+        <>
+          <h1 className="font-orbitron">Create Event</h1>
+          <form onSubmit={submitHandler} className="flex flex-col items-center gap-2 my-8">
+            <div className="flex flex-col gap-2 w-[80%]">
+              <input
+                required
+                onChange={inputHandler}
+                className="input text-gray-400"
+                name="eventName"
+                type="text"
+                placeholder="Event Name"
+              />
+              <input
+                onChange={inputHandler}
+                className="input text-gray-400"
+                name="eventDate"
+                type="date"
+                placeholder="Event Date"
+              />
+              <input
+                onChange={inputHandler}
+                className="input text-gray-400"
+                name="ticketPrice"
+                type="number"
+                placeholder="Ticket Price"
+              />
+              <input
+                onChange={inputHandler}
+                className="input text-gray-400"
+                name="ticketQuantity"
+                type="number"
+                placeholder="Ticket Quantity"
+              />
+              <input
+                onChange={inputHandler}
+                className="input text-gray-400"
+                name="artistName"
+                type="text"
+                placeholder="Artist Name"
+              />
+              <input
+                onChange={inputHandler}
+                className="input text-gray-400"
+                name="artistAddress"
+                type="text"
+                placeholder="Artist Wallet Address"
+              />
+            </div>
+            <Button full>Create</Button>
+          </form>
+        </>
+      )}
     </div>
   );
 };
