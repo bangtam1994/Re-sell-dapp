93dcf476309075383ff8c2656207e265e722a071
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-12-09 13:47:03 +0100

    landingpage styling

--- frontend/packages/nextjs/pages/index.tsx
+++ frontend/packages/nextjs/pages/index.tsx
@@ -1,20 +1,22 @@
-import Link from "next/link";
+import Image from "next/image";
 import type { NextPage } from "next";
-import { Button } from "~~/components/Button";
 import { MetaHeader } from "~~/components/MetaHeader";
 
 const Home: NextPage = () => {
   return (
     <>
       <MetaHeader />
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <h1>ALLOW NFTS TO SOLVE YOUR RESELL PROBLEM</h1>
-        <h2>Create your event without limitations</h2>
-        <div>
-          <Button link="/create-event">Create New Event</Button>
-          <Button link="/events">Existing Events</Button>
-          <Button link="/events/my-events">My Events</Button>
-          <Button link="/marketplace-resale">Resell Marketplace</Button>
+      <div className="select-none md:flex text-center md:text-left md:justify-between md:mx-10 lg:mx-16 xl:mx-48 2xl:mx-96 mt-10">
+        <div className="flex flex-col tracking-wide">
+          <p className="text-4xl md:text-7xl font-medium m-0">ALLOW NFTS TO</p>
+          <p className="text-4xl md:text-7xl font-medium m-0">SOLVE YOUR</p>
+          <p className="text-4xl md:text-7xl font-medium m-0">RESALE</p>
+          <p className="text-4xl md:text-7xl font-medium m-0">PROBLEM</p>
+          <p className="text-2xl md:text-2xl my-8 tracking-widest">Create your event without limitations</p>
+        </div>
+        <div className="hidden md:block relative">
+          <Image className="absolute rotate-45 right-10" src={"/git_2.gif"} alt="gif2" width={135} height={135} />
+          <Image className="mt-20" src={"/git_1.gif"} alt="gif1" width={300} height={300} />
         </div>
       </div>
     </>
