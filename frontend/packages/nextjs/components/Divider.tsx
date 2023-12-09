303d98656c4e95cd994def9a7301de8df7a19d40
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-12-09 14:12:26 +0100

    landing page connected to wallet

---
+++ frontend/packages/nextjs/components/Divider.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+export const Divider = ({ children }: { children: ReactNode }) => {
+  return (
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <div style={{ borderBottom: "1px solid white", width: "100%" }} />
+      <span className="py-8 w-full text-center">{children}</span>
+      <div style={{ borderBottom: "1px solid white", width: "100%" }} />
+    </div>
+  );
+};
--- frontend/packages/nextjs/pages/index.tsx
+++ frontend/packages/nextjs/pages/index.tsx
@@ -1,24 +1,36 @@
 import Image from "next/image";
 import type { NextPage } from "next";
+import { useAccount } from "wagmi";
+import { Divider } from "~~/components/Divider";
 import { MetaHeader } from "~~/components/MetaHeader";
 
 const Home: NextPage = () => {
+  const { address, isConnecting, isDisconnected } = useAccount();
   return (
     <>
       <MetaHeader />
-      <div className="select-none md:flex text-center md:text-left md:justify-between md:mx-10 lg:mx-16 xl:mx-48 2xl:mx-96 mt-10">
-        <div className="flex flex-col tracking-wide">
-          <p className="text-4xl md:text-7xl font-medium m-0">ALLOW NFTS TO</p>
-          <p className="text-4xl md:text-7xl font-medium m-0">SOLVE YOUR</p>
-          <p className="text-4xl md:text-7xl font-medium m-0">RESALE</p>
-          <p className="text-4xl md:text-7xl font-medium m-0">PROBLEM</p>
-          <p className="text-2xl md:text-2xl my-8 tracking-widest">Create your event without limitations</p>
+      {isDisconnected ? (
+        <div className="select-none text-center md:flex md:text-left md:justify-between md:mx-10 lg:mx-16 xl:mx-48 2xl:mx-96 mt-10">
+          <div className="flex flex-col tracking-wide">
+            <p className="text-4xl md:text-7xl font-medium m-0">ALLOW NFTS TO</p>
+            <p className="text-4xl md:text-7xl font-medium m-0">SOLVE YOUR</p>
+            <p className="text-4xl md:text-7xl font-medium m-0">RESALE</p>
+            <p className="text-4xl md:text-7xl font-medium m-0">PROBLEM</p>
+            <p className="text-2xl md:text-2xl my-8 tracking-widest">Create your event without limitations</p>
+          </div>
+          <div className="hidden md:block relative">
+            <Image className="absolute rotate-45 right-10" src={"/git_2.gif"} alt="gif2" width={135} height={135} />
+            <Image className="mt-20" src={"/git_1.gif"} alt="gif1" width={300} height={300} />
+          </div>
         </div>
-        <div className="hidden md:block relative">
-          <Image className="absolute rotate-45 right-10" src={"/git_2.gif"} alt="gif2" width={135} height={135} />
-          <Image className="mt-20" src={"/git_1.gif"} alt="gif1" width={300} height={300} />
+      ) : (
+        <div className="mx-4 md:mx-10 lg:mx-16 xl:mx-48 2xl:mx-96 mt-10">
+          <p className="text-center text-4xl md:text-3xl font-medium m-0">EXISTING EVENTS</p>
+          <Divider>EVENTS I CREATED</Divider>
+          <Divider>EVENTS I AM ATTENDING</Divider>
+          <Divider>ALL OTHER EVENTS</Divider>
         </div>
-      </div>
+      )}
     </>
   );
 };
