1356fcdff95ee9203c5aa4ce8788230660fa2e69
Kelith <garofalodavide7@gmail.com>
2023-12-12 19:39:10 +0100

    fixed resell/buy ticket api

--- backend/resell-dapp-api/src/events/event.controller.ts
+++ backend/resell-dapp-api/src/events/event.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Param, Query, Controller, Post, Put, HttpStatus, HttpException } from '@nestjs/common';
+import {
+  Body,
+  Param,
+  Query,
+  Controller,
+  Post,
+  Put,
+  HttpStatus,
+  HttpException,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateEventDto } from './dto/create-event.dto';
 import { TicketDto } from './dto/ticket.dto';
@@ -16,42 +25,29 @@ export class EventController {
   }
 
   // Adds a ticket to an event (passing the event contract address as a parameter)
-  @Put('/:contractAddress/ticket')
+  @Put('/:eventAddress/ticket')
   async addTicket(
-    @Param('contractAddress') contractAddress: string,
-    @Body() ticketDto?: TicketDto,
-    @Body('buyerAddress') buyerAddress?: string,
+    @Param('eventAddress') eventAddress: string,
+    @Body() ticketDto: TicketDto,
   ) {
-    if (ticketDto) {
-      return this.eventService.createTicket(ticketDto, contractAddress);
-    }
-    else if (buyerAddress) {
-      return this.eventService.buyTicket(buyerAddress, contractAddress);
-    }
-    else {
-      throw new HttpException({
-        status: HttpStatus.BAD_REQUEST,
-        error: 'No ticket or buyer address provided',
-      }, HttpStatus.BAD_REQUEST);
-    }
+    return this.eventService.createTicket(ticketDto, eventAddress);
   }
 
   // Resells a ticket (passing the event contract address as a parameter, the owner address and the quantity as query parameters)
-  @Put('/:contractAddress/resell-ticket')
-  async resellTicket(
-    @Query('ownerAddress') ownerAddress: string,
-    @Query('quantity') quantity: number,
-    @Param('contractAddress') contractAddress: string,    
+  @Put('/:eventAddress/update-ticket')
+  async updateTicket(
+    @Param('eventAddress') eventAddress: string,
+    @Body() ticketDto: TicketDto,
   ) {
-    return this.eventService.resellTicket(ownerAddress, quantity, contractAddress);
+    return this.eventService.updateTicket(eventAddress, ticketDto);
   }
 
   // Buys a ticket (passing the event contract address as a parameter, the buyer address as a query parameter)
-  // @Put('/:contractAddress/buy-ticket')
+  // @Put('/:eventAddress/buy-ticket')
   // async buyTicket(
   //   @Query('buyerAddress') buyerAddress: string,
-  //   @Param('contractAddress') contractAddress: string,
+  //   @Param('eventAddress') eventAddress: string,
   // ) {
-  //   return this.eventService.buyTicket(buyerAddress, contractAddress);
+  //   return this.eventService.buyTicket(buyerAddress, eventAddress);
   // }
 }
--- backend/resell-dapp-api/src/events/event.service.ts
+++ backend/resell-dapp-api/src/events/event.service.ts
@@ -5,11 +5,9 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Event } from './schemas/event.schema';
 import { CreateEventDto } from './dto/create-event.dto';
 import { TicketDto } from './dto/ticket.dto';
-import e from 'express';
 
 @Injectable()
 export class EventService {
-
   constructor(@InjectModel(Event.name) private eventModel: Model<Event>) {}
 
   async createEvent(createEventDto: CreateEventDto): Promise<Event> {
@@ -17,90 +15,126 @@ export class EventService {
       const createdEvent = new this.eventModel(createEventDto);
       return await createdEvent.save();
     } catch (error) {
-      throw new HttpException({
-        status: HttpStatus.INTERNAL_SERVER_ERROR,
-        error: 'An error occurred while creating the event',
-      }, HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'An error occurred while creating the event',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 
-  async createTicket(ticketDto: TicketDto, contractAddress: string): Promise<Event> {
-    const event = await this.eventModel.findOne({ contractAddress: contractAddress });
+  async createTicket(
+    ticketDto: TicketDto,
+    eventAddress: string,
+  ): Promise<Event> {
+    const event = await this.eventModel.findOne({
+      contractAddress: eventAddress,
+    });
     if (event) {
       // check if ticket already exists
-      const ticket = event.ticketList.find(ticket => ticket.ticket_address === ticketDto.ticket_address);
+      const ticket = event.ticketList.find(
+        (ticket) => ticket.ticket_address === ticketDto.ticket_address,
+      );
       if (ticket) {
-        throw new HttpException({
-          status: HttpStatus.BAD_REQUEST,
-          error: 'Ticket already exists',
-        }, HttpStatus.BAD_REQUEST);
+        throw new HttpException(
+          {
+            status: HttpStatus.BAD_REQUEST,
+            error: 'Ticket already exists',
+          },
+          HttpStatus.BAD_REQUEST,
+        );
       }
       // check if the length of the ticket list is equal to the quantity of the event
       if (event.ticketList.length === event.quantity) {
-        throw new HttpException({
-          status: HttpStatus.BAD_REQUEST,
-          error: 'No more tickets available',
-        }, HttpStatus.BAD_REQUEST);
+        throw new HttpException(
+          {
+            status: HttpStatus.BAD_REQUEST,
+            error: 'No more tickets available',
+          },
+          HttpStatus.BAD_REQUEST,
+        );
       }
 
       event.ticketList.push(ticketDto);
       return await event.save();
-    }
-    else {
-      throw new HttpException({
-        status: HttpStatus.NOT_FOUND,
-        error: 'Event not found',
-      }, HttpStatus.NOT_FOUND);
-    }
-  }
-
-  async resellTicket(ownerAddress: string, quantity: number, contractAddress: string) {
-    const event = await this.eventModel.findOne({ contractAddress: contractAddress });
-    if (event) {
-      const tickets = event.ticketList.filter(ticket => ticket.owner_address === ownerAddress);
-      if (tickets.length < quantity) {
-        throw new HttpException({
-          status: HttpStatus.BAD_REQUEST,
-          error: 'Not enough tickets',
-        }, HttpStatus.BAD_REQUEST);
-      }
-      else {
-        // change the onSale property of the tickets to true
-        tickets.forEach(ticket => ticket.onSale = true);
-        return await event.save();
-      }
-    }
-    else {
-      throw new HttpException({
-        status: HttpStatus.NOT_FOUND,
-        error: 'Event not found',
-      }, HttpStatus.NOT_FOUND);
-    }
-  }
-
-  async buyTicket(buyerAddress: string, contractAddress: string) {
-    // check if at least one of the tickets of the event is on sale
-    const event = await this.eventModel.findOne({ contractAddress: contractAddress });
-    if (event) {
-      const tickets = event.ticketList.filter(ticket => ticket.onSale === true);
-      if (tickets.length === 0 ) {
-        throw new HttpException({
+    } else {
+      throw new HttpException(
+        {
           status: HttpStatus.NOT_FOUND,
-          error: 'No tickets on sale',
-        }, HttpStatus.NOT_FOUND);
-      } 
-      else {
-        // change the owner address of the first ticket on sale to the buyer address
-        tickets[0].owner_address = buyerAddress;
-        tickets[0].onSale = false;
-        return await event.save();
-      }
-    }
-    else {
-      throw new HttpException({
-        status: HttpStatus.NOT_FOUND,
-        error: 'Event not found',
-      }, HttpStatus.NOT_FOUND);
+          error: 'Event not found',
+        },
+        HttpStatus.NOT_FOUND,
+      );
     }
   }
+
+  async updateTicket(
+    eventAddress: string,
+    ticketDto: TicketDto,
+  ): Promise<Event> {
+    const event = await this.eventModel.findOne({
+      contractAddress: eventAddress,
+    });
+    if (event) {
+      const ticket = event.ticketList.find(
+        (ticket) => ticket.ticket_address === ticketDto.ticket_address,
+      );
+      if (ticket) {
+        ticket.owner_address = ticketDto.owner_address;
+        ticket.onSale = ticketDto.onSale;
+        ticket.price = ticketDto.price;
+        ticket.date = ticketDto.date;
+        return await event.save();
+      } else {
+        throw new HttpException(
+          {
+            status: HttpStatus.NOT_FOUND,
+            error: 'Ticket not found',
+          },
+          HttpStatus.NOT_FOUND,
+        );
+      }
+    } else {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: 'Event not found',
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+  }
+  // ) {
+  //   const event = await this.eventModel.findOne({
+  //     eventAddress: eventAddress,
+  //   });
+  //   if (event) {
+  //     const tickets = event.ticketList.filter(
+  //       (ticket) => ticket.owner_address === ownerAddress,
+  //     );
+  //     if (tickets.length < quantity) {
+  //       throw new HttpException(
+  //         {
+  //           status: HttpStatus.BAD_REQUEST,
+  //           error: 'Not enough tickets',
+  //         },
+  //         HttpStatus.BAD_REQUEST,
+  //       );
+  //     } else {
+  //       change the onSale property of the tickets to true
+  //       tickets.forEach((ticket) => (ticket.onSale = true));
+  //       return await event.save();
+  //     }
+  //   } else {
+  //     throw new HttpException(
+  //       {
+  //         status: HttpStatus.NOT_FOUND,
+  //         error: 'Event not found',
+  //       },
+  //       HttpStatus.NOT_FOUND,
+  //     );
+  //   }
+  // }
 }
